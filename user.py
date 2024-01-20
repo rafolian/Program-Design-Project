82161c63ecdf20bcdc76645a1d6fb85ec656fca6
Amirah Nailofar <1231102231@student.mmu.edu.my>
2024-01-20 23:27:27 +0800

    username registration for players

---
+++ user.py
@@ -0,0 +1,15 @@
+# *********************************************************
+# Program: user.py
+# Course: PSP0101 PROBLEM SOLVING AND PROGRAM DESIGN
+# Class: TL??
+# Year: 2023/24 Trimester 1
+# Names: MEMBER_NAME_1 | MEMBER_NAME_2 | MEMBER_NAME_3
+# IDs: MEMBER_ID_1 | MEMBER_ID_2 | MEMBER_ID_3
+# Emails: MEMBER_EMAIL_1 | MEMBER_EMAIL_2 | MEMBER_EMAIL_3
+# Phones: MEMBER_PHONE_1 | MEMBER_PHONE_2 | MEMBER_PHONE_3
+# *********************************************************
+
+def register_user():
+    username = input("Enter your username: ")
+    print(f"Welcome {username}! Let's start the Mind Puzzler.")
+    return username
